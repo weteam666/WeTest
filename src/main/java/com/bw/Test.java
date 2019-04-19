aaad0b5440dbcd94ca27fe1605e235c1496b2d7b
Jacky-YC <1206290545@qq.com>
2019-04-19 22:22:46 +0800

    修改项目jdk版本为 11

--- src/main/java/com/bw/Test.java
+++ src/main/java/com/bw/Test.java
@@ -1,4 +1,11 @@
 package com.bw;
 
+import java.util.List;
+
 public class Test {
+    public static void main(String[] args) {
+        var list = List.of("a", "b", "c");
+        list.stream().forEach(System.out::println);
+    }
+
 }
